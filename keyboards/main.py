a81e0766e0ddfa6e073e7d5e276abc833a5f6f9b
m1laCHka <milanamakedonskaa6@gmail.com>
2026-08-18 14:22:30 +0300

    Delete keyboards directory

--- keyboards/main.py
+++
@@ -1,9 +0,0 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
-
-main_menu = ReplyKeyboardMarkup(
-    keyboard=[
-        [KeyboardButton(text="👤 Профиль"), KeyboardButton(text="🛒 Магазин")],
-        [KeyboardButton(text="🎮 Игры"), KeyboardButton(text="🏆 Топы")]
-    ],
-    resize_keyboard=True
-)

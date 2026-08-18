8f7dc7481e5edf5a488201e0982cbdac1ecd2bb1
m1laCHka <milanamakedonskaa6@gmail.com>
2026-08-18 11:39:59 +0300

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,3 @@
+START_HELP_PHOTO = "https://i.ibb.co/N6dqh7MQ/5953fda8-0711-46b4-90ab-80f1fc2955f3.jpg"
+FEMALE_PROFILE_PHOTO = "https://i.ibb.co/RMbs94m/584b2c22-ff9b-491f-bccf-63fa7e692f6a.jpg"
+MALE_PROFILE_PHOTO = "https://i.ibb.co/pv7by3Y9/7e592789-6a08-4897-bfaa-054df3735f95.jpg"
